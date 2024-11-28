d0ce46adc08a32c4c5860e463312d97fdef1e494
Raul Almanza <ra.almanzaserrano@gmail.com>
2024-11-28 16:30:54 -0600

    Implement a session button to control login and logout

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { CiLogout } from 'react-icons/ci';
 import { SidebarItem } from './SidebarItem';
 import {
   IoBasketOutline,
@@ -12,7 +11,8 @@ import {
 } from 'react-icons/io5';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
-import { redirect } from 'next/navigation';
+
+import { SessionButton } from './SessionButton';
 
 const menuItems = [
   {
@@ -125,10 +125,7 @@ export const Sidebar = async () => {
       </div>
 
       <div className="px-6 -mx-6 pt-4 flex justify-between items-center border-t">
-        <button className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-200 group">
-          <CiLogout />
-          <span className="group-hover:text-white">Logout</span>
-        </button>
+        <SessionButton />
       </div>
     </aside>
   );
