3e498037b1aa29e3414f682ac85cc56cb1254546
Raul Almanza <ra.almanzaserrano@gmail.com>
2024-11-28 14:46:38 -0600

    Get and display the information of the authenticated user in the server side

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -9,6 +9,9 @@ import {
   IoCodeWorkingOutline,
   IoListOutline,
 } from 'react-icons/io5';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { redirect } from 'next/navigation';
 
 const menuItems = [
   {
@@ -38,7 +41,11 @@ const menuItems = [
   },
 ];
 
-export const Sidebar = () => {
+export const Sidebar = async () => {
+  const session = await getServerSession(authOptions);
+  const userName = session?.user?.name ?? 'No name provided';
+  const userImage = session?.user?.image ?? 'https://randomuser.me/api/portraits/men/1.jpg';
+
   return (
     <aside className="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-black transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
       <div>
@@ -94,15 +101,13 @@ export const Sidebar = () => {
 
         <div className="mt-8 text-center">
           <Image
-            src="https://randomuser.me/api/portraits/men/1.jpg"
+            src={userImage}
             alt=""
             className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28"
             width={150}
             height={150}
           />
-          <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">
-            Cynthia J. Watts
-          </h5>
+          <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">{userName}</h5>
           <span className="hidden text-gray-400 lg:block">Admin</span>
         </div>
 
